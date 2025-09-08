b80a67bce58f17426e9aff880c14ec6ca9195110
aaleksaaleksic <aaleksic9722rn@raf.rs>
2025-09-08 19:48:43 +0200

    [BE]add Initial backend setup

---
+++ backend/readify/src/main/java/me/remontada/readify/ReadifyApplication.java
@@ -0,0 +1,13 @@
+package me.remontada.readify;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ReadifyApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ReadifyApplication.class, args);
+	}
+
+}
