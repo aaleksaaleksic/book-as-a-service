5f70cb2cedbe1df55c3976c15bb63f87d354550c
aaleksaaleksic <aaleksic9722rn@raf.rs>
2025-09-25 22:16:09 +0200

    UČITANA PRVA STRANA

--- frontend/next.config.ts
+++ frontend/next.config.ts
@@ -1,7 +1,31 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  /* config options here */
+  webpack: (config, { isServer }) => {
+    config.resolve.alias = {
+      ...config.resolve.alias,
+      canvas: false,
+    };
+
+    // Fix for react-pdf in SSR
+    if (isServer) {
+      config.resolve.alias = {
+        ...config.resolve.alias,
+        'react-pdf': false,
+        'pdfjs-dist': false,
+      };
+    }
+
+    return config;
+  },
+  async rewrites() {
+    return [
+      {
+        source: '/api/:path*',
+        destination: 'http://localhost:8080/api/:path*',
+      },
+    ];
+  },
 };
 
 export default nextConfig;
